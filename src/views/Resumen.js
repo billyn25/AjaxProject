30590117c67ab1651aae509630547ad0fe589216
tr07.wfsd24.ene19 <tr07.wfsd24.ene19@bootcamp-upgrade.com>
2021-01-02 17:44:52 +0100

    fixed html

--- src/views/Resumen.js
+++ src/views/Resumen.js
@@ -226,11 +226,11 @@ function Resumen({datos, reset}) {
                         <div className="bg-transparent w-100 mt-4 mb-3">
                             <form id="form" className="row g-3 justify-content-start align-items-center">
                                 <div className=" ml-4 mr-3">
-                                    <img className="imgHiper" src="http://antenaszalla.com/img/hp.png" width="140" height="52"/>
+                                    <img className="imgHiper" src="https://antenaszalla.com/img/hp.png" width="140" height="52"/>
                                 </div>
-                                <div className="col-md-3 text-secondary">
+                                <div className="col-md-3">
                                     <input type="text" className="inputCustomer form-control" id="inputName"
-                                           placeholder="Nombre/Empresa"/>
+                                           placeholder="Cliente/Empresa"/>
                                 </div>
                                 <div className="col-md-2">
                                     <input type="number" className="inputCustomer form-control" id="inputMovil"
