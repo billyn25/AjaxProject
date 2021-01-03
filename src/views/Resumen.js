c50124fc49978749a830edba77ff8960cd05ab02
tr07.wfsd24.ene19 <tr07.wfsd24.ene19@bootcamp-upgrade.com>
2021-01-03 11:30:31 +0100

    fixex view mobile

--- src/views/Resumen.js
+++ src/views/Resumen.js
@@ -224,7 +224,7 @@ function Resumen({datos, reset}) {
                     </div>
                     <div className="table-responsive-sm" id='divTable'>
                         <hr className="bg-white mb-0 mt-0"/>
-                        <form id="form" className="pt-3 pb-3 row g-3 justify-content-start align-items-center">
+                        <form id="form" className="pt-2 pb-2 row g-3 justify-content-between align-items-center">
                             <div className="ml-3 mr-4">
                                 <img id="imgHiperId" className="imgHiper" src={hp}/>
                             </div>
