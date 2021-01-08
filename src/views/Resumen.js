c42bcbb0353646947dcd354ed3a67e2dd8fb5522
tr07.wfsd24.ene19 <tr07.wfsd24.ene19@bootcamp-upgrade.com>
2021-01-08 15:25:37 +0100

    css fixed

--- src/views/Resumen.js
+++ src/views/Resumen.js
@@ -208,7 +208,7 @@ function Resumen({datos, reset}) {
             margin: 0,
             filename: 'AjaxConfig.pdf',
             image: {type: 'png', quality: 0.9},
-            html2canvas: { scale:2, width:830, scrollX: 20, scrollY: 0, dpi: 192, letterRendering: true, useCORS: true},
+            html2canvas: { scale:2, width:845, scrollX: 25, scrollY: 0, dpi: 192, letterRendering: true, useCORS: true},
             jsPDF: {unit: 'pt', format: 'a4', orientation: 'p'}
         };
 
